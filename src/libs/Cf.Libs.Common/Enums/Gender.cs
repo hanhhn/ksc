1ffdfb6ee7f6c68808275f691b4c83f66f628556
Hoàng Ngọc Hạnh <hngochanh@outlook.com>
2019-06-21 12:07:55 +0700

    remove

--- src/libs/Cf.Libs.Common/Enums/Gender.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Cf.Libs.Common.Enums
-{
-    public enum Gender
-    {
-        Man = 10,
-        Woman = 20,
-        Other = 30
-    }
-}
--- src/libs/Cf.Libs.Core/Caching/BaseCache/CachingDefaults.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Cf.Libs.Core.Caching.BaseCache
-{
-    public class CachingDefaults
-    {
-        public static int CacheTime => 60;
-        public static string RedisDataProtectionKey => "DataProtectionKeys";
-    }
-}

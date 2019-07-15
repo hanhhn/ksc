f0034afe6bd07c9d4c7457db7ae5805782e82696
Hoàng Ngọc Hạnh <hngochanh@outlook.com>
2019-07-15 15:00:24 +0700

    update

--- src/api/Cf.Ksc/Program.cs
+++ src/api/Cf.Ksc/Program.cs
@@ -13,37 +13,13 @@ namespace Cf.Ksc
     {
         public static void Main(string[] args)
         {
-            var host = CreateWebHostBuilder(args).Build();
-
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                var context = services.GetRequiredService<ApplicationDbContext>();
-                context.Database.Migrate();
-                //https://docs.microsoft.com/en-us/aspnet/core/security/authorization/secure-data?view=aspnetcore-2.2
-            }
-
-            host.Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-            .UseKestrel()
+            //.UseKestrel()
             .UseContentRoot(Directory.GetCurrentDirectory())
-            //.ConfigureAppConfiguration((hostingContext, config) =>
-            //{
-            //    var env = hostingContext.HostingEnvironment;
-            //    config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-            //          .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
-            //    config.AddEnvironmentVariables();
-            //})
-            //.ConfigureLogging((hostingContext, logging) =>
-            //{
-            //    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
-            //    logging.AddConsole();
-            //    logging.AddDebug();
-            //    logging.AddEventSourceLogger();
-            //})
             .UseIISIntegration()
             .UseStartup<Startup>();
     }
